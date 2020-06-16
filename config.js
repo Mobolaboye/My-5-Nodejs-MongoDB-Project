44b382d875dd32b9c0bb51bea731d642f5530f74
Bolanle Adeboye <adeboyeomobolanle@gmail.com>
2020-06-16 12:04:32 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '950116315436933',
+        clientSecret: 'ff658a059164e7cc0f0fc5507b400f45'
+    }
 }
